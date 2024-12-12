3e0c6ec65d693d92829ccbd0f10c5db4805f3d67
Mingbing-get <1508850533@qq.com>
2024-12-12 17:20:16 +0800

    "提交时间: Thu Dec 12 2024 17:20:16 GMT+0800 (China Standard Time)"

--- components/src/container/index.tsx
+++ components/src/container/index.tsx
@@ -1,11 +1,18 @@
-import { EasyCoderElement } from '@easy-coder/sdk/store'
+import { useMemo } from 'react'
+import { EasyCoderElement, useElementContext, useStateById } from '@easy-coder/sdk/store'
 
 export interface ContainerProps extends EasyCoderElement.DataProps {
   style?: React.CSSProperties
   className?: string
-  children?: () => React.ReactNode
+  list?: any[]
+  children?: (payload?: { item: any }) => React.ReactNode
 }
 
-export default function Container({ children, ...extra }: ContainerProps) {
-  return <div {...extra}>{children?.()}</div>
+export default function Container({ children, list, ...extra }: ContainerProps) {
+  const { id } = useElementContext<unknown, ContainerProps>()
+  const element = useStateById('element', id)
+
+  const hasList = useMemo(() => !!element?.props?.list, [element?.props?.list])
+
+  return <div {...extra}>{hasList ? list?.map((item) => children?.({ item })) : children?.()}</div>
 }
--- components/src/container/meta.ts
+++ components/src/container/meta.ts
@@ -2,6 +2,30 @@ import { EasyCoderElement } from '@easy-coder/sdk/store'
 
 import Container, { ContainerProps } from '.'
 
+const createSlotPayloadType: EasyCoderElement.DynamicVariable<ContainerProps> = async (props, getAttrType) => {
+  const listType = await getAttrType('list')
+  if (!listType) return
+
+  if (listType.type === 'array') {
+    return {
+      label: '当前项',
+      ...listType.item,
+    }
+  } else if (listType.type === 'multipleEnum') {
+    return {
+      label: '当前项',
+      type: 'enum',
+      enumGroupName: listType.enumGroupName,
+    }
+  } else if (listType.type === 'multipleLookup') {
+    return {
+      label: '当前项',
+      type: 'lookup',
+      modalName: listType.modalName,
+    }
+  }
+}
+
 const containerMeta: EasyCoderElement.Desc<ContainerProps> = {
   type: 'system_component_container',
   label: '容器',
@@ -15,9 +39,19 @@ const containerMeta: EasyCoderElement.Desc<ContainerProps> = {
       label: '样式',
     },
   },
+  attr: {
+    list: {
+      type: 'ref',
+      label: '遍历数据',
+      canAcceptTypes: ['array', 'multipleEnum', 'multipleLookup'],
+    },
+  },
   slot: {
     children: {
       label: '子节点',
+      payload: {
+        item: createSlotPayloadType,
+      },
     },
   },
   Render: Container,
